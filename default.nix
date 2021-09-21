d558edaa0bddbb7cc2e01f77fd181ee58aed8110
Gabriella Gonzalez <Gabriel439@gmail.com>
2021-09-20 18:13:26 -0700

    Fix nix-shell for `grace` sub-package

---
+++ default.nix
@@ -0,0 +1,31 @@
+let
+  nixpkgs = builtins.fetchTarball {
+    url    = "https://github.com/NixOS/nixpkgs/archive/e0ce3c683ae677cf5aab597d645520cddd13392b.tar.gz";
+    sha256 = "08ans3w6r4fbs1wx6lzlp4xwhy6p04x3spvvrjz5950w8mzxqm61";
+  };
+
+  overlay = pkgsNew: pkgsOld: {
+    haskellPackages = pkgsOld.haskellPackages.override (old: {
+      overrides =
+        let
+          oldOverrides = old.overrides or (_: _: {});
+
+          sourceOverrides = pkgsNew.haskell.lib.packageSourceOverrides {
+            grace = ./grace;
+
+            grace-core = ./grace-core;
+          };
+
+        in
+          pkgsNew.lib.composeExtensions oldOverrides sourceOverrides;
+    });
+  };
+
+  pkgs = import nixpkgs {
+    config = { allowBroken = true; };
+
+    overlays = [ overlay ];
+  };
+
+in
+  { inherit (pkgs.haskellPackages) grace-core grace; }
