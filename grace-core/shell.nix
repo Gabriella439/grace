61f1ab64951b5b418ca88eedf7a9389974dd0b38
Gabriella Gonzalez <Gabriel439@gmail.com>
2021-09-20 17:13:18 -0700

    Fix `./grace-core/shell.nix` (#18)

--- grace-core/shell.nix
+++ grace-core/shell.nix
@@ -2,4 +2,4 @@ let
   pkgs = import <nixpkgs> { config = { allowBroken = true; }; overlays = []; };
 
 in
-  (pkgs.haskellPackages.callCabal2nix "grace" ./. { }).env
+  (pkgs.haskellPackages.callCabal2nix "grace-core" ./. { }).env
