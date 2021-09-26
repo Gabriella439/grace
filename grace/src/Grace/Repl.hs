164038e0277c2c9f08c9e4528d493d316e85bb76
Gabriella Gonzalez <Gabriel439@gmail.com>
2021-09-26 11:16:15 -0700

    Use `RecordWildCards` to simplify `Grace.REPL` module

--- grace/src/Grace/Repl.hs
+++ grace/src/Grace/Repl.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE FlexibleContexts  #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeApplications  #-}
+{-# LANGUAGE RecordWildCards   #-}
 
 -- | This module contains the implementation of the @grace repl@ subcommand
 
@@ -38,17 +38,23 @@ repl :: IO ()
 repl = do
     manager <- TLS.newTlsManager
 
-    let prompt MultiLine  = return "... "
-        prompt SingleLine = return ">>> "
+    let interpret input = do
+            context <- get
 
-    let interpret string = do
+            Except.runExceptT (Interpret.interpretWith context Nothing manager input)
+
+    let err e =
+            liftIO (Text.IO.hPutStrLn IO.stderr (pack (displayException e)))
+
+    let command string = do
             let input = Code "(input)" (pack string)
 
-            context <- get
-            eitherResult <- Except.runExceptT (Interpret.interpretWith context Nothing manager input)
+            eitherResult <- interpret input
 
             case eitherResult of
-                Left e -> liftIO (Text.IO.hPutStrLn IO.stderr (pack (displayException e)))
+                Left e -> do
+                    err e
+
                 Right (_inferred, value) -> do
                     let syntax = Normalize.quote [] value
 
@@ -57,66 +63,63 @@ repl = do
                     liftIO (Pretty.renderIO True width IO.stdout (Pretty.pretty syntax <> "\n"))
 
     let assignment string
-            | (var, '=' : expr) <- break (== '=') string
-            = do
-              let input = Code "(input)" (pack expr)
+            | (var, '=' : expr) <- break (== '=') string = do
+                let input = Code "(input)" (pack expr)
 
-              let variable = strip (pack var)
+                let variable = strip (pack var)
 
-              context <- get
+                eitherResult <- interpret input
 
-              eitherResult <- do
-                  Except.runExceptT (Interpret.interpretWith context Nothing manager input)
+                case eitherResult of
+                    Left e -> do
+                        err e
 
-              case eitherResult of
-                  Left e -> liftIO (Text.IO.hPutStrLn IO.stderr (pack (displayException e)))
-                  Right (type_, value) -> State.modify ((variable, type_, value) :)
+                    Right (type_, value) -> do
+                        State.modify ((variable, type_, value) :)
 
-            | otherwise
-            = liftIO (putStrLn "usage: let = {expression}")
+            | otherwise = do
+                liftIO (putStrLn "usage: let = {expression}")
 
     let infer expr = do
             let input = Code "(input)" (pack expr)
 
-            context <- get
-
-            eitherResult <- do
-                Except.runExceptT (Interpret.interpretWith context Nothing manager input)
+            eitherResult <- interpret input
 
             case eitherResult of
                 Left e -> do
-                    liftIO (Text.IO.hPutStrLn IO.stderr (pack (displayException e)))
+                    err e
 
                 Right (type_, _) -> do
                     width <- liftIO Pretty.getWidth
 
                     liftIO (Pretty.renderIO True width IO.stdout (Pretty.pretty type_ <> "\n"))
 
-    let commands =
+    let options =
             [ ("let", Repline.dontCrash . assignment)
             , ("type", Repline.dontCrash . infer)
             ]
 
-    let complete =
+    let tabComplete =
             Custom (Repline.runMatcher [ (":", completeCommands) ] completeIdentifiers)
           where
             completeCommands =
-                Repline.listCompleter (fmap adapt commands)
+                Repline.listCompleter (fmap adapt options)
               where
                 adapt (c, _) = ":" <> c
 
             completeIdentifiers = Repline.listCompleter (fmap unpack (toList reserved))
 
-    let action =
-            Repline.evalReplOpts ReplOpts
-                { banner = prompt
-                , command = interpret
-                , options = commands
-                , prefix = Just ':'
-                , multilineCommand = Just "paste"
-                , tabComplete = complete
-                , initialiser = return ()
-                , finaliser = return Repline.Exit
-                }
+    let banner MultiLine  = return "... "
+        banner SingleLine = return ">>> "
+
+    let prefix = Just ':'
+
+    let multilineCommand = Just "paste"
+
+    let initialiser = return ()
+
+    let finaliser = return Repline.Exit
+
+    let action = Repline.evalReplOpts ReplOpts{..}
 
     State.evalStateT action []
