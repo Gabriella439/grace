4907ea881f5624e1a7db7d862d50cac8a34f8eca
Gabriella Gonzalez <Gabriel439@gmail.com>
2022-05-25 18:07:54 -0700

    Add tutorial to "Grace browser"

--- try-grace/Main.hs
+++ try-grace/Main.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE NamedFieldPuns    #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE MultiWayIf        #-}
+{-# LANGUAGE QuasiQuotes       #-}
 
 module Main where
 
@@ -36,6 +37,7 @@ import qualified Data.IORef as IORef
 import qualified Data.JSString as JSString
 import qualified Data.Scientific as Scientific
 import qualified Data.Sequence as Seq
+import qualified Data.String.Interpolate as Interpolate
 import qualified Data.Text as Text
 import qualified Data.Text.Lazy as Text.Lazy
 import qualified Data.Text.Lazy.Encoding as Text.Encoding
@@ -698,9 +700,10 @@ renderInput _ _ = do
 
 main :: IO ()
 main = do
-    input  <- getElementById "input"
-    output <- getElementById "output"
-    error  <- getElementById "error"
+    input    <- getElementById "input"
+    output   <- getElementById "output"
+    error    <- getElementById "error"
+    tutorial <- getElementById "tutorial"
 
     spinner <- createElement "div"
 
@@ -741,7 +744,10 @@ main = do
 
             if  | Text.null text -> do
                     setError ""
+
                 | otherwise -> do
+                    setDisplay tutorial "none"
+
                     let input_ = Code "(input)" text
 
                     replaceChild error spinner
@@ -754,9 +760,315 @@ main = do
                         Right (type_, value) -> do
                             setOutput type_ value
 
-    callback <- Callback.asyncCallback interpret
+    inputCallback <- Callback.asyncCallback interpret
 
-    addEventListener input "input" callback
+    addEventListener input "input" inputCallback
+
+    tutorialCallback <- Callback.asyncCallback do
+        setTextContent input tutorialText
+
+        setAttribute input "style" "width: 100%; max-width: 100%; height: 20em;"
+
+        interpret
+
+    addEventListener tutorial "click" tutorialCallback
 
     Monad.when exists do
         interpret
+
+tutorialText :: Text
+tutorialText = [Interpolate.i|\# This is an interactive tutorial for the Fall-from-Grace language (a.k.a.
+\# "Grace" for short).  Feel free to make this text area larger so that you can
+\# more easily read the code.
+\#
+\# This is not a complete tour of the Grace language, but rather just enough of a
+\# tour to pique your interest.
+\#
+\# First off, any line prefixed with a "\#" character is a comment, like this one.
+\#
+\# Grace does not support special syntax for multi-line comments.  Instead, you
+\# have to prefix each comment line with a single-line comment.
+{ \# A Grace record begins with an opening brace (i.e. "{") and ends with a
+  \# closing brace (i.e. "}").  This tutorial is implemented inside of one large
+  \# Grace record.
+  \#
+  \# Records have zero or more fields and each field is associated with a value.
+  \# For example, the first field in our record is named "Example Bool" and the
+  \# associated value is `true`:
+  "Example Bool": true
+
+, \# Fields are separated by commas, such as the comma preceding this comment.
+  \# Grace is not whitespace-sensitive, so commas can be present at the
+  \# beginning or end of a line.  This tutorial uses the "leading comma"
+  \# convention.
+  \#
+  \# Our next field is named "Example number", which is associated with the value
+  \# `1`.  Note that each field of a record may store a different type of value.
+  \# For example, the field named "Example Bool" stores a Bool value (e.g.
+  \# true) whereas the field named "Example number stores a Natural number (e.g.
+  \# 1):
+  "Example number": 1
+
+, \# Immediately after this code you'll see the HTML output of this code.  The
+  \# Grace expression inside of this text area is converted to the equivalent
+  \# web page.  For example, the field named "Example Bool" that stores the value
+  \# true is rendered as a text label ("Example Bool") followed by the Bool
+  \# value rendered as a checkbox (which is checked, because the value was true).
+  \#
+  \# This next field stores some Text, which is also rendered as Text when
+  \# converted to HTML.  Try to modify the text to replace "{name}" with your
+  \# name.  As you edit the text, watch how the HTML output updates in response.
+  \#
+  \# After doing, change the value of the "Example Bool" field above from true to
+  \# false.  This will uncheck the matching checkbox below.
+  "Example text": "My name is {name}"
+
+, \# This web page is interactive in two different ways:
+  \#
+  \# * The HTML output interactively updates whenever you change the code in this
+  \#   box
+  \#
+  \# * Any function you create is turned into an interactive web form
+  \#
+  \# We've already seen the first type of interactivity: when we edit this code
+  \# the HTML output changes.  Now let's illustrate the latter type of
+  \# interactivity by having the following field store a function.  This will
+  \# create a web form underneath the "Example function" label with a text input
+  \# and a text output.  Edit the text input underneath the "Example function"
+  \# label to enter your name and watch the matching text output update as you do
+  \# so.
+  "Example function": \\name -> "My name is " + name
+
+, \# This page generates inputs and outputs that intelligently match the
+  \# inferred type of the code.  For example, we will annotate the following
+  \# function with this type signature:
+  \#
+  \#     Bool -> List Bool
+  \#
+  \# … which means that our function has a single input (of type Bool) and
+  \# a single output (a List of Bool)s.  The corresponding web form underneath
+  \# the "Triplicate Bool" label will prompt the user with a single checkbox for
+  \# input and the output will be a list of three checkboxes whose values all
+  \# match the input:
+  "Triplicate Bool": (\\x -> [ x, x, x ]) : Bool -> List Bool
+
+, \# By default, if multiple types of values would work then this live demo
+  \# will infer a type of JSON.  For example, if we omit the type annotation as
+  \# in the following example then the interpreter will infer this more general
+  \# type:
+  \#
+  \#     forall (a : Type) . a -> List a
+  \#
+  \# … and then for the purpose of generating a form it will replace the
+  \# arbitrary type "a" with JSON, producing this more specific type:
+  \#
+  \#     JSON -> List JSON
+  \#
+  \# … so the following example will differ from the prior example by prompting
+  \# the user for a JSON input and then producing three matching copies of that
+  \# JSON as output.  Try replacing the "null" input underneat the "Triplicate
+  \# JSON" label with "[ 1, true ]" and see what happens:
+  "Triplicate JSON": \\x -> [ x, x, x ]
+
+, \# In fact, Grace is a superset of JSON, meaning that any JSON expression is
+  \# syntactically legal.  For some JSON expressions they will work without a
+  \# type annotation if the interpreter can infer a sensible type for them, like
+  \# this one:
+  "Example JSON":
+    {
+      "clients": [
+        {
+          "isActive": true,
+          "age": 36,
+          "name": "Dunlap Hubbard",
+          "email": "dunlaphubbard@cedward.com",
+          "phone": "+1 (890) 543-2508"
+        },
+        {
+          "isActive": true,
+          "age": 24,
+          "name": "Kirsten Sellers",
+          "email": "kirstensellers@emergent.com",
+          "phone": "+1 (831) 564-2190"
+        }
+      ]
+    }
+
+, \# However, even weirder JSON expressions will still type-check if you add an
+  \# explicit JSON type annotation to them.  However, there's a tradeoff:
+  \# expressions of type JSON are easier to produce, but harder to consume
+  \# because the type-checker cannot provide as many guarantees about what they
+  \# contain:
+  "Weird example JSON":
+    [ [ 1, true ], { x: "ABC" } ] : JSON
+
+  \# Grace supports importing arbitrary subexpressions by URL.  For example,
+  \# we can import a US federal tax income calculator for 2022 by uncommenting
+  \# following "Tax calculator" field containing a URL for that calculator.  The
+  \# field is commented out by default to keep the demo snappier when making code
+  \# changes and you will want to comment this out again when you're done with
+  \# this example:
+  \# , "Tax calculator": https://gist.githubusercontent.com/Gabriella439/712d0648bbdcfcc83eadd0ee394beed3/raw/1b03f661577521b4d3dc6ca73dd11475a30c1594/incomeTax.ffg
+
+, \# Now let's do a quick tour of all the simple (scalar) types that we can
+  \# convert to HTML outputs:
+  "Sample scalar values":
+    { "Sample Bool values": [ false, true ]
+    , "Sample Natural numbers": [ 0, 1, 2 ]
+    , "Sample Integers": [ -2, -1, 0, 1, 2 ]
+    , "Sample Real numbers": [ -2.1, 3.14159265, 2.718281828459 ]
+    , "Sample Text strings": [ "ABC", "Hello, world!" ]
+    , "Sample JSON values": [ 1, true, [ "ABC", null ] ] : List JSON
+    }
+
+, \# We can also create more complex data structures, like:
+  "Sample complex data structures":
+    { "Sample record": { "Name": "John Doe", "Age": 24 }
+    , "Sample list": [ 1, 2, 3, 4, 5 ]
+    , "Sample optional values": [ null, 1 ]
+    }
+
+, \# Note that all values in a list have to have the same type.  For example,
+  \# try replacing the number 2 in the following list with a true and you'll
+  \# get a type error.  Then change the element back to 2 when you're done:
+  "List of numbers": [ 1, 2 ]
+
+, \# There are two common ways to store values of different types in the same
+  \# list.  The first (and most general way) is to wrap each element in a
+  \# union alternative, which is any identifier beginning with a capital letter,
+  \# like this:
+  "List of union alternatives": [ Left 1, Right true ]
+
+, \# The second (and more limited way) is that if all the values are valid
+  \# JSON, then you can add a type annotation declaring the list elements to all
+  \# be JSON and then the type-checker will accept that:
+  "List of JSON": [ 1, true ] : List JSON
+
+, \# You can also nest complex types arbitrarily, like this:
+  "Sample nested data structures":
+    { "List of lists": [ [ 1, 2 ], [ 3, 4, 5 ] ]
+    , "Record of records":
+        { "Position": { "x": 1.4, "y": -2.1 }
+        , "Status": { "Health": 100, "Turn": 4 }
+        }
+    , "List of records":
+        [ { "Name": "John Doe", "Grade": 93 }
+        , { "Name": "Mary Jane", "Grade": 95 }
+        ]
+    , "List of unions of records":
+        [ GitHub
+            { "Repository":
+                "https://github.com/Gabriel439/Haskell-Turtle-Library.git"
+            , "Revision": "ae5edf227b515b34c1cb6c89d9c58ea0eece12d5"
+            }
+        , Local { "Relative path": "~/proj/optparse-applicative" }
+        , Local { "Relative path": "~/proj/discrimination" }
+        , Hackage { "Package": "lens", "Version": "4.15.4" }
+        , GitHub
+            { "Repository": "https://github.com/haskell/text.git"
+            , "Revision": "ccbfabedea1cf5b38ff19f37549feaf01225e537"
+            }
+        , Local { "Relative path": "~/proj/servant-swagger" }
+        , Hackage { "Package": "aeson", "Version": "1.2.3.0" }
+        ]
+    }
+
+, \# All of these types work as function inputs, too.
+  \#
+  \# In Grace, you write a user-defined function using this syntax:
+  \#
+  \#     \\input -> output
+  \#
+  \# … where "input" is the name of the function input and "output" is the
+  \# "body" of the function (i.e. the function's result), which can refer to the
+  \# function's input.
+  \#
+  \# For example, the following function has this inferred type:
+  \#
+  \#    { "x": Bool, "y": Bool } -> { "x || y": Bool }
+  \#
+  \# … meaning that the function takes a record with two fields as input and
+  \# produces a record with one field as output.  The generated form will
+  \# match the inferred type.  In particular, the input section will have two
+  \# fields labeled "x" and "y" (since our input record has two fields named
+  \# "x" and "y"), and each field will be associated with a checkbox (since our
+  \# input record fields both had type Bool).
+  "Boolean OR": \\record -> { "x || y": record.x || record.y }
+
+, \# Here is a quick tour of all of the supported input types, which we'll
+  \# illustrate by creating a trivial function for each input type that returns
+  \# the input as the output:
+  "Sample inputs and outputs":
+    { "Bool input and output": \\x -> x : Bool
+    , "Numeric input and output": \\x -> x : Real
+    , "Text input and output": \\x -> x : Text
+    , "JSON input and output": \\x -> x : JSON
+    , "Optional input and output": \\x -> x : Optional Bool
+    , "List input and output": \\x -> x : List Bool
+    , "Record input and output": \\x -> x : { a: Bool, b: Bool }
+    , "Union input and output": \\x -> x : < Left : Bool | Right: Natural >
+    }
+
+, \# Not everything can be sensibly converted to HTML and when that happens the
+  \# result will just render the code.  For example, the following function will
+  \# just render as code:
+  "Function that renders as plain code": \\f x -> f x
+
+, \# However, the interpreter will still try to intelligently do something when
+  \# possible.  For example, try entering ", " in the text input underneath the
+  \# "Partially interpreted function" example and watch what happens to the
+  \# rendered code:
+  "Partially interpreted function":
+    \\separator function -> (function "ABC" + separator + function "DEF") : Text
+
+, \# Grace supports let expressions, which mean that you can define intermediate
+  \# values anywhere within Grace code; even in the middle of a record like this
+  \# one.  For example:
+  "Example let expression":
+    let x = 2
+
+    in  x + x
+
+, \# Intermediate expressions can be plain data (such as the "x" in the previous
+  \# example), or functions, like this:
+  "Examplet let-defined function":
+    let twice = \\x -> [ x, x ]
+    in  twice 2
+
+, \# Grace also has the following built-in functions, many of which you can
+  \# directly test below using the generated form for that function:
+  "Built-in functions":
+    { "Real/equal": Real/equal
+    , "Real/lessThan": Real/lessThan
+    , "Real/negate": Real/negate
+    , "Real/show": Real/show
+    , "List/drop": List/drop
+    , "List/equal": List/equal
+    , "List/fold": List/fold
+    , "List/equal": List/equal
+    , "List/head": List/head
+    , "List/indexed": List/indexed
+    , "List/last": List/last
+    , "List/length": List/length
+    , "List/map": List/map
+    , "List/reverse": List/reverse
+    , "List/take": List/take
+    , "Integer/even": Integer/even
+    , "Integer/negate": Integer/negate
+    , "Integer/odd": Integer/odd
+    , "Integer/abs": Integer/abs
+    , "JSON/fold": JSON/fold
+    , "Natural/fold": Natural/fold
+    , "Text/equal": Text/equal
+    }
+
+  \# Grace also has a Prelude of utility functions which are derived from
+  \# built-in functions.  You can test-drive the Prelude by commenting out the
+  \# following field:
+  \# , "Grace Prelude": https://raw.githubusercontent.com/Gabriella439/grace/main/prelude/package.ffg
+
+  \# That's the end of this tutorial.  Have fun and if you want to learn more
+  \# about Grace, visit the project on GitHub here:
+  \#
+  \# https://github.com/Gabriella439/grace\#grace
+}|]
