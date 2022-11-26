6902d356d0736c91ad1161e3e11ee17fe5e5adb9
Gabriella Gonzalez <Gabriel439@gmail.com>
2022-11-25 17:59:20 -0800

    Add garnix settings to flake.nix

--- flake.nix
+++ flake.nix
@@ -132,4 +132,12 @@
 
         devShell = devShells.default;
       });
+
+  nixConfig = {
+    extra-substituters = [ "https://cache.garnix.io" ];
+
+    extra-trusted-public-keys = [
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+    ];
+  };
 }
