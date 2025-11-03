Gabriella439/grace

commit 9640bdc06ae5bbaeef00884a9aa4f5ca4fd203c9

Add support for destructuring `let` bindings (#163)
* Rename `Binding` to `Assignment`

* Rename `NameBinding` to `Binding`

* Rename `FieldName` to `NameBinding`

* Implement `PlainBinding` in terms of `NameBinding`

* Add support for destructuring record bindings

* Add tests

* Fix try-grace build

* Update examples and prompts

src/Grace/Parser.hs
@@ -53,10 +53,10 @@ import Text.Earley (Grammar, Prod, Report(..), rule, (<?>))
 import Text.Megaparsec (ParseErrorBundle(..), State(..), try)
 
 import Grace.Syntax
-    ( Binding(..)
+    ( Assignment(..)
+    , Binding(..)
     , Chunks(..)
     , Field(..)
-    , FieldName(..)
     , NameBinding(..)
     , Smaller(..)
     , Syntax(..)
@@ -883,7 +883,7 @@ render t = case t of
 
 grammar :: Bool -> Grammar r (Parser r (Syntax Offset Input))
 grammar endsWithBrace = mdo
-    nameBinding <- rule do
+    parseBinding <- rule do
         let annotated = do
                 parseToken Grace.Parser.OpenParenthesis
 
@@ -905,16 +905,25 @@ grammar endsWithBrace = mdo
 
                 parseToken Grace.Parser.CloseParenthesis
 
-                pure NameBinding{ nameLocation, name, annotation, assignment }
+                pure PlainBinding
+                    { plain = NameBinding
+                        { nameLocation
+                        , name
+                        , annotation
+                        , assignment
+                        }
+                    }
 
         let unannotated = do
                 ~(nameLocation, name) <- locatedLabel
 
-                pure NameBinding
-                    { nameLocation
-                    , name
-                    , annotation = Nothing
-                    , assignment = Nothing
+                pure PlainBinding
+                    { plain = NameBinding
+                        { nameLocation
+                        , name
+                        , annotation = Nothing
+                        , assignment = Nothing
+                        }
                     }
 
         let fields = do
@@ -925,7 +934,7 @@ grammar endsWithBrace = mdo
 
                         pure annotation
 
-                let parseAssignment = do
+                let parseDefault = do
                         parseToken Grace.Parser.Equals
 
                         assignment <- expression
@@ -933,50 +942,53 @@ grammar endsWithBrace = mdo
                         pure assignment
 
                 let parseFieldName = do
-                        ~(fieldNameLocation, name) <- locatedRecordLabel
+                        ~(nameLocation, name) <- locatedRecordLabel
 
                         annotation <- optional parseAnnotation
 
-                        assignment <- optional parseAssignment
+                        assignment <- optional parseDefault
 
-                        return FieldName{ fieldNameLocation, name, annotation, assignment }
+                        return NameBinding{ nameLocation, name, annotation, assignment }
 
                 fieldNamesLocation <- locatedToken Grace.Parser.OpenBrace
 
                 fieldNames <- parseFieldName `sepBy` parseToken Grace.Parser.Comma
                 parseToken Grace.Parser.CloseBrace
 
-                pure FieldNamesBinding{ fieldNamesLocation, fieldNames }
+                pure RecordBinding{ fieldNamesLocation, fieldNames }
 
         annotated <|> unannotated <|> fields
 
     expression <- rule
         (   do  location <- locatedToken Grace.Parser.Lambda
 
-                nameBindings <- some1 nameBinding
+                bindings <- some1 parseBinding
 
                 parseToken Grace.Parser.Arrow
 
                 body0 <- expression
 
                 return do
-                    let cons nameBinding_ body = Syntax.Lambda
+                    let cons binding body = Syntax.Lambda
                             { location
-                            , nameBinding = nameBinding_
+                            , binding
                             , body
                             }
-                    foldr cons body0 nameBindings
 
-        <|> do  bindings <- some1 binding
+                    foldr cons body0 bindings
+
+        <|> do  assignments <- some1 parseAssignment
 
                 parseToken Grace.Parser.In
 
                 body <- expression
 
                 return do
-                    let Syntax.Binding{ nameLocation = location } =
-                            NonEmpty.head bindings
-                    Syntax.Let{ location, bindings, body }
+                    let location = case NonEmpty.head assignments of
+                            Syntax.Definition{ nameLocation } -> nameLocation
+                            Syntax.Bind{ binding = Syntax.PlainBinding{ plain = Syntax.NameBinding{ nameLocation } } } -> nameLocation
+                            Syntax.Bind{ binding = Syntax.RecordBinding{ fieldNamesLocation } } -> fieldNamesLocation
+                    Syntax.Let{ location, assignments, body }
 
         <|> do  location <- locatedToken Grace.Parser.If
 
@@ -1339,47 +1351,48 @@ grammar endsWithBrace = mdo
                 return e
         )
 
-    binding <- rule
-        (   do  nameLocation <- locatedToken Grace.Parser.Let
+    parseAssignment <- rule do
+        _ <- parseToken Grace.Parser.Let
 
-                name <- label
+        let parseDefinition = do
+                ~(nameLocation, name) <- locatedLabel
 
-                nameBindings <- many nameBinding
+                bindings <- many parseBinding
 
-                parseToken Grace.Parser.Equals
+                annotation <- optional do
+                    parseToken Grace.Parser.Colon
 
-                assignment <- expression
+                    t <- quantifiedType
 
-                return Syntax.Binding
-                    { nameLocation
-                    , name
-                    , nameBindings
-                    , annotation = Nothing
-                    , assignment
-                    }
-
-        <|> do  nameLocation <- locatedToken Grace.Parser.Let
-
-                name <- label
-
-                nameBindings <- many nameBinding
-
-                parseToken Grace.Parser.Colon
-
-                annotation <- fmap Just quantifiedType
+                    return t
 
                 parseToken Grace.Parser.Equals
 
                 assignment <- expression
 
-                return Syntax.Binding
+                return Syntax.Definition
                     { nameLocation
                     , name
-                    , nameBindings
+                    , bindings
                     , annotation
                     , assignment
                     }
-        )
+
+        let parseBind = do
+                binding <- parseBinding
+
+                parseToken Grace.Parser.Equals
+
+                assignment <- expression
+
+                return Syntax.Bind
+                    { binding
+                    , assignment
+                    }
+
+        assignment <- parseDefinition <|> parseBind
+
+        return assignment
 
     recordLabel <- rule (reservedLabel <|> label <|> alternative <|> text)
 

doctest/Main.hs
@@ -17,6 +17,7 @@ main = Test.DocTest.doctest
     , "-XDeriveTraversable"
     , "-XDerivingStrategies"
     , "-XDuplicateRecordFields"
+    , "-XFlexibleContexts"
     , "-XGeneralizedNewtypeDeriving"
     , "-XMultiWayIf"
     , "-XNamedFieldPuns"

examples/poem.ffg
@@ -21,10 +21,10 @@ let generatePoem idea =
                 }
 
         let renderedStructure = "
-                - Form: ${structure.form}
-                - Stanzas: ${show (structure.stanzaCount : Text)}
-                - Lines per stanza: ${show (structure.linesPerStanza : Text)}
-                - Rhyme scheme: ${structure.rhymeScheme}
+                - Form: ${structure."Form"}
+                - Stanzas: ${show (structure."Stanza Count" : Natural)}
+                - Lines per stanza: ${show (structure."Lines per Stanza" : Natural)}
+                - Rhyme scheme: ${structure."Rhyme Scheme"}
                 "
 
         let style = prompt
@@ -39,9 +39,9 @@ let generatePoem idea =
                 }
 
         let renderedStyle = "
-                - Tone: ${style.tone}
-                - Voice: ${style.voice}
-                - Literary Devices: ${concatSep ", " style.literaryDevices}
+                - Tone: ${style."Tone"}
+                - Voice: ${style."Voice"}
+                - Literary Devices: ${concatSep ", " style."Literary Devices"}
                 "
 
         let poem = prompt

examples/prompt.ffg
@@ -2,6 +2,6 @@
 
 let key = .'OpenAI API key'
 
-let numbers = prompt{ key, text: "Give me two numbers" }
+let { x, y } = prompt{ key, text: "Give me two numbers" }
 
-in  { x: numbers.x, y: numbers.y, sum: numbers.x + numbers.y : Integer }
+in  { x, y, sum: x + y : Integer }

src/Grace/Normalize.hs
@@ -132,13 +132,13 @@ evaluate keyToMethods env₀ syntax₀ = do
                 argument' <- loop env argument
                 apply keyToMethods function' argument'
 
-            Syntax.Lambda{ nameBinding = Syntax.NameBinding{ name, assignment }, body } -> do
+            Syntax.Lambda{ binding = Syntax.PlainBinding{ plain = Syntax.NameBinding{ name, assignment } }, body } -> do
                 newAssignment <- traverse (loop env) assignment
 
                 pure (Value.Lambda env (Value.Name name newAssignment) body)
 
-            Syntax.Lambda{ nameBinding = Syntax.FieldNamesBinding{ fieldNames }, body } -> do
-                let process Syntax.FieldName{ name, assignment } = do
+            Syntax.Lambda{ binding = Syntax.RecordBinding{ fieldNames }, body } -> do
+                let process Syntax.NameBinding{ name, assignment } = do
                         newAssignment <- traverse (loop env) assignment
 
                         return (name, newAssignment)
@@ -164,20 +164,58 @@ evaluate keyToMethods env₀ syntax₀ = do
 
                     promote newAnnotated annotation
 
-            Syntax.Let{ bindings, body = body₀ } -> do
-                newEnv <- Monad.foldM snoc env bindings
+            Syntax.Let{ assignments, body = body₀ } -> do
+                newEnv <- Monad.foldM snoc env assignments
 
                 loop newEnv body₀
               where
-                snoc environment Syntax.Binding{ nameLocation, name, nameBindings, assignment } = do
-                    let cons nameBinding body =
-                            Syntax.Lambda{ location = nameLocation, nameBinding, body }
+                snoc environment Syntax.Definition{ nameLocation, name, bindings, assignment } = do
+                    let cons binding body =
+                            Syntax.Lambda{ location = nameLocation, binding, body }
 
-                    let newAssignment = foldr cons assignment nameBindings
+                    let newAssignment = foldr cons assignment bindings
 
                     value <- loop environment newAssignment
 
                     return ((name, value) : environment)
+                snoc environment Syntax.Bind{ binding, assignment = assignment₀ } = do
+                    value₀ <- loop environment assignment₀
+
+                    case binding of
+                        -- TODO: This is ignoring the `assignment` field of
+                        -- `NameBinding`
+                        Syntax.PlainBinding{ plain = Syntax.NameBinding{ name } } -> do
+                            return ((name, value₀) : environment)
+
+                        Syntax.RecordBinding{ fieldNames } -> do
+                            case value₀ of
+                                Value.Record hashMap -> do
+                                    let process Syntax.NameBinding{ name, assignment = assignment₁} = do
+                                            value <- case HashMap.lookup name hashMap of
+                                                Nothing -> case assignment₁ of
+                                                    Nothing -> do
+                                                        return (Value.Scalar Syntax.Null)
+
+                                                    Just a -> do
+                                                        loop environment a
+
+                                                Just (Value.Scalar Syntax.Null) -> case assignment₁ of
+                                                    Nothing -> do
+                                                        return (Value.Scalar Syntax.Null)
+
+                                                    Just a -> do
+                                                        loop environment a
+
+                                                Just value -> do
+                                                    return value
+
+                                            return (name, value)
+
+                                    entries <- traverse process fieldNames
+
+                                    return (entries <> environment)
+                                _ -> do
+                                    error "Grace.Normalize.evaluate: non-records can't be destructured as records"
 
             Syntax.List{ elements } -> do
                 values <- traverse (loop env) elements
@@ -838,23 +876,25 @@ quote value = case value of
     Value.Lambda env names_ body₀ ->
         foldl snoc newLambda env
       where
-        nameBinding = case names_ of
+        binding = case names_ of
             Value.Name name assignment ->
-                Syntax.NameBinding
-                    { nameLocation = location
-                    , name
-                    , annotation = Nothing
-                    , assignment = fmap quote assignment
+                Syntax.PlainBinding
+                    { plain = Syntax.NameBinding
+                        { nameLocation = location
+                        , name
+                        , annotation = Nothing
+                        , assignment = fmap quote assignment
+                        }
                     }
 
             Value.FieldNames fieldNames ->
-                Syntax.FieldNamesBinding
+                Syntax.RecordBinding
                     { fieldNamesLocation = location
                     , fieldNames = do
-                        (fieldName, assignment) <- fieldNames
-                        return Syntax.FieldName
-                            { name = fieldName
-                            , fieldNameLocation = location
+                        (name, assignment) <- fieldNames
+                        return Syntax.NameBinding
+                            { nameLocation = location
+                            , name
                             , annotation = Nothing
                             , assignment = fmap quote assignment
                             }
@@ -862,29 +902,29 @@ quote value = case value of
 
         newLambda = Syntax.Lambda
             { location
-            , nameBinding
+            , binding
             , body = first (\_ -> location) body₀
             }
 
-        toBinding n v = Syntax.Binding
+        toBinding n v = Syntax.Definition
             { name = n
             , nameLocation = location
-            , nameBindings = []
+            , bindings = []
             , annotation = Nothing
             , assignment = quote v
             }
 
-        snoc e@Syntax.Let{ bindings = b :| bs, body = body₁ } (n, v)
+        snoc e@Syntax.Let{ assignments = a :| as, body = body₁ } (n, v)
             | Syntax.usedIn n e = Syntax.Let
                 { location
-                , bindings = toBinding n v :| (b : bs)
+                , assignments = toBinding n v :| (a : as)
                 , body = body₁
                 }
             | otherwise = e
         snoc e (n, v)
             | Syntax.usedIn n e = Syntax.Let
                 { location
-                , bindings = toBinding n v :| []
+                , assignments = toBinding n v :| []
                 , body = e
                 }
             | otherwise = e

tasty/data/complex/destructure-record-input.ffg
@@ -1,0 +1,4 @@
+let { a, b = 1, c: Natural, d: Natural = 1, e, f = 1, g: Natural = 1 }
+      = { a: 2, b: 2, c: 2, d: 2 }
+
+in  { a, b, c, d, e, f, g }

commit ecc64d66d16996014cc58ed522de65889d1c813a

Fix parsing performance regression
In #163 I inadvertently introduced a parsing performance regression
where a series of `let` assignments of the form:

```
let x₀ = e₀
let x₁ = e₁
…
let xₙ = eₙ
in  …
```

… would lead to an exponential blow-up in the parser.  This is because
these sorts of `let` assignments could be parsed as either a
`Syntax.Definition` or a `Syntax.Bind` and the parser would parse both
possible alternatives in parallel for each assignment.  That in turn
meant that if you had N such assignments then the parser would be
parsing 2^N branches in parallel, leading to the exponential blowup.

The fix is to amend the parser to unambiguously parse these sorts of
assignments as `Sytnax.Definition`s and not `Syntax.Bind`s.

src/Grace/Parser.hs
@@ -903,7 +903,19 @@ data REPLCommand
 
 grammar :: GrammaticalForm a -> Grammar r (Parser r a)
 grammar form = mdo
-    parseBinding <- rule do
+    parseUnadornedBinding <- rule do
+        ~(nameLocation, name) <- locatedLabel
+
+        pure PlainBinding
+            { plain = NameBinding
+                { nameLocation
+                , name
+                , annotation = Nothing
+                , assignment = Nothing
+                }
+            }
+
+    parseAdornedBinding <- rule do
         let annotated = do
                 parseToken Grace.Parser.OpenParenthesis
 
@@ -931,18 +943,6 @@ grammar form = mdo
                         , name
                         , annotation
                         , assignment
-                        }
-                    }
-
-        let unannotated = do
-                ~(nameLocation, name) <- locatedLabel
-
-                pure PlainBinding
-                    { plain = NameBinding
-                        { nameLocation
-                        , name
-                        , annotation = Nothing
-                        , assignment = Nothing
                         }
                     }
 
@@ -977,7 +977,9 @@ grammar form = mdo
 
                 pure RecordBinding{ fieldNamesLocation, fieldNames }
 
-        annotated <|> unannotated <|> fields
+        annotated <|> fields
+
+    parseBinding <- rule (parseUnadornedBinding <|> parseAdornedBinding)
 
     expression <- rule
         (   do  location <- locatedToken Grace.Parser.Lambda
@@ -1371,7 +1373,7 @@ grammar form = mdo
                 return e
         )
 
-    parseREPLCommand <- rule do
+    parseLetAssignment <- rule do
         let parseDefinition = do
                 ~(nameLocation, name) <- locatedLabel
 
@@ -1400,7 +1402,15 @@ grammar form = mdo
                     }
 
         let parseBind = do
-                binding <- parseBinding
+                -- We don't use `parseBinding` here because otherwise
+                -- `parseDefinition` and `parseBind` will overlap for
+                -- assignments of the form `let x = e …` and then the parser
+                -- will double the number of branches it tracks for each such
+                -- assignment, leading to an exponential blowup in the parser.
+                -- Using `parseAdornedBinding` here ensures that every `let`
+                -- assignment is parsed unambiguously as a `Definition` or
+                -- `Bind`.
+                binding <- parseAdornedBinding
 
                 parseToken Grace.Parser.Equals
 
@@ -1436,7 +1446,7 @@ grammar form = mdo
                     , assignment
                     }
 
-        parseREPLCommand <|> parseForAssignment
+        parseLetAssignment <|> parseForAssignment
 
     recordLabel <- rule (reservedLabel <|> label <|> alternative <|> text)
 
@@ -1680,7 +1690,7 @@ grammar form = mdo
         return a
 
     replCommand <- rule
-        (fmap Evaluate expression <|> fmap Assign parseREPLCommand)
+        (fmap Evaluate expression <|> fmap Assign parseLetAssignment)
 
     return case form of
         Expression continuation ->